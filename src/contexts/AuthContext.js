6c1ca65f62ed608ace85b2ede75373ecdf6feefe
xR0v3Rx <infoashu.k@gmail.com>
2021-08-08 20:54:11 +0530

    all set

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState,useEffect} from 'react';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import {auth} from "../firebase";
 
 const AuthContext = React.createContext();
@@ -8,19 +8,22 @@ export const useAuth=()=>useContext(AuthContext);
 
 export const AuthProvider =({children})=>{
     const [loading, setloading] = useState(true);
-    const [user,setUser] = useState({});
+    const [user,setUser] = useState(null);
     const history =useHistory();
 
     useEffect(()=>{
         auth.onAuthStateChanged((user)=>{
             setUser(user);
             setloading(false);
-            history.push('/chats');
+
+            if(user) history.push('/chats');
         })
     },[user,history]);
+
     const value ={user};
+    
     return(
-        <AuthContext.Provider value ={value}>
+        <AuthContext.Provider value={value}>
             {!loading && children}
         </AuthContext.Provider>
     )
